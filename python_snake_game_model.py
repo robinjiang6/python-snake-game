c3ac16e45dc602205a88535768f83ad4d40e1389
Robin Jiang <robinjiang6@gmail.com>
2022-12-08 21:36:48 -0800

    started snake game

---
+++ python_snake_game_model.py
@@ -0,0 +1,5 @@
+# python_snake_game_model.py
+# author: Robin Jiang
+
+# this module contains the logic or "model" part of the snake game
+
---
+++ python_snake_game_view.py
@@ -0,0 +1,6 @@
+# python_snake_game_view.py
+# author: Robin Jiang
+
+# this module contains the gui or "view" of the snake game
+
+import pygame
